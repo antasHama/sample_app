4e3f7705cdb5130e9040c984ba47222855cb8eca
b.Lkhamsuren <ant.lkhamsuren@gmail.com>
2019-11-20 09:54:12 +0900

    test pr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world!"
+    render html: "hello, world! FOR Open FR"
   end
+
+  def index
+  end
+
+  def show
+  end
+
 end
